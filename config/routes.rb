5b2653cf6b26241e3b182749f8e0db44d6d39133
OTAKU_G <gt689229@gmail.com>
2015-03-30 18:35:15 +0800

    設定 root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 	get "welcome/say_hello" => "welcome#say"
 	get "welcome" => "welcome#index"
 
+	root :to => "welcome#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
