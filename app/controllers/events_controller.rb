54b693fdf08b25da3f492a3acb6e2a9403bcd539
OTAKU_G <gt689229@gmail.com>
2015-04-08 16:40:01 +0800

    新增 respond_to

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,13 @@
 class EventsController < ApplicationController
    def index
       @events = Event.all
+
+      respond_to do |format|
+         format.html
+         format.xml { render :xml => @events.to_xml}
+         format.json { render :json => @events.to_json}
+         format.atom { @feed_title = "My event list" }
+      end
    end
 
    def new
@@ -16,6 +23,12 @@ class EventsController < ApplicationController
 
    def show
       @event = Event.find(params[:id])
+      
+      respond_to do |format|
+         format.html { @page_title = @event.name }
+         format.xml
+         format.json { render :json => { id: @event.id, name: @event.name }.to_json }
+      end
    end
 
    def edit
---
+++ app/views/events/show.xml.builder
@@ -0,0 +1,4 @@
+xml.event do |e|
+   e.name @event.name
+   e.description @event.description
+end
